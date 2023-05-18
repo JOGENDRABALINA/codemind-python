418bc119a70a7555b492b3b2e63160736e079c24
JOGENDRABALINA <121618316+JOGENDRABALINA@users.noreply.github.com>
2023-05-18 21:12:14 +0530

    def is_perfect_number(number):     divisors_sum = 0     for i in range(1, number):         if number % i == 0:             divisors_sum += i     if divisors_sum == number:         return True     else:         return False number = int(input())
    if is_perfect_number(number):
        print(True)
    else:
        print(False)

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,9 +1,15 @@
-n=int(input())
-sum=0
-for i in range(1,n//2+1):
-    if n%i==0:
-        sum=sum+i
-if sum==n:
-    print('True')
+def is_perfect_number(number):
+    divisors_sum = 0
+    for i in range(1, number):
+        if number % i == 0:
+            divisors_sum += i
+    if divisors_sum == number:
+        return True
+    else:
+        return False
+number = int(input())
+
+if is_perfect_number(number):
+    print(True)
 else:
-    print('False')
+    print(False)
