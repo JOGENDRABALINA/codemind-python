60ed2deb4132e20a0d9f729d6a8ccaf8b2a8468b
JOGENDRABALINA <121618316+JOGENDRABALINA@users.noreply.github.com>
2023-05-19 20:31:48 +0530

    def is_happy_number(number):
        seen_numbers = set()
        while number != 1:
            if number in seen_numbers:
                return False
            seen_numbers.add(number)
            next_number = 0
            while number > 0:
                digit = number % 10
                next_number += digit * digit
                number //= 10
            
            number = next_number
        
        return True
    if __name__ == "__main__":
        number = int(input())
        if is_happy_number(number):
            print(True)
        else:
            print(False)

--- happy_number.py
+++ happy_number.py
@@ -1,16 +1,28 @@
-def is_happy_number(num):
-    
+def is_happy_number(number):
+
     seen_numbers = set()
+    while number != 1:
+        if number in seen_numbers:
+            return False
+        seen_numbers.add(number)
+        next_number = 0
+        while number > 0:
+            digit = number % 10
+            next_number += digit * digit
+            number //= 10
+        
+        number = next_number
     
-    while num != 1 and num not in seen_numbers:
-        seen_numbers.add(num)
-        num = sum(int(digit) ** 2 for digit in str(num))
-    
-    return num == 1
+    return True
+if __name__ == "__main__":
+    number = int(input())
+    if is_happy_number(number):
+        print(True)
+    else:
+        print(False)
 
 
-number = int(input())
-if is_happy_number(number):
-    print(True)
-else:
-    print(False)
+
+
+
+
