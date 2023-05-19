896acc6119c35672b32e724dfd03db6aeff2649b
JOGENDRABALINA <121618316+JOGENDRABALINA@users.noreply.github.com>
2023-05-19 20:01:11 +0530

    def find_largest_digit(number):     number_str = str(number)     largest_digit = int(number_str[0])     for digit in number_str:         current_digit = int(digit)         if current_digit > largest_digit:             largest_digit = current_digit
        return largest_digit
    if __name__ == "__main__":
        number = int(input())
        largest_digit = find_largest_digit(number)
        print(largest_digit)

--- Largest_Digit.py
+++ Largest_Digit.py
@@ -1,14 +1,13 @@
-def find_largest_digit(num):
+def find_largest_digit(number):
+    number_str = str(number)
+    largest_digit = int(number_str[0])
+    for digit in number_str:
+        current_digit = int(digit)
+        if current_digit > largest_digit:
+            largest_digit = current_digit
     
-    num_str = str(num)
-    max_digit = 0
-    for digit in num_str:
-        if int(digit) > max_digit:
-            max_digit = int(digit)
-    return max_digit
-
-
-number = int(input())
-largest_digit = find_largest_digit(number)
-
-print(largest_digit)
+    return largest_digit
+if __name__ == "__main__":
+    number = int(input())
+    largest_digit = find_largest_digit(number)
+    print(largest_digit)
