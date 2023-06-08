e490782b30f8297dac4ffe79b7e2da5cd8b21c46
BALINA JOGENDRA VENKATA SIVA SUBRAHMANYAM <121618316+JOGENDRABALINA@users.noreply.github.com>
2023-06-08 10:26:54 +0530

    Add files via upload

---
+++ class work programs for loop.py
@@ -0,0 +1,81 @@
+##for i in range(1,11):
+##    print('JOGENDRA')
+
+##for i in range(1,101):
+##    print(i,end='\t')
+
+##i=10
+##while i<=10:
+##    print(i)
+##    i=i+1
+
+##for i in range(3,9,2):
+##    print(i)
+
+##for i in range(10,20,30):
+##    print(i)
+
+##for i in range(101):
+##    print(i)
+
+##-------------->Range is same for all these 3
+##1.for i in range(0,10):
+##    print(i)
+##
+##2.for i in range(0,10,1):
+##    print(i)
+##
+##3.for i in range(10):
+##   print(i)
+
+##for i in range(1,101):
+##      print(i,end=' ')
+
+##for i in range(1,101):
+##      print(i,end='\n')
+
+##for i in range(1,10):
+##      print(i,end=' @')
+
+##a=int(input("Enter the number: "))
+##b=int(input("Enter the number: "))
+##for i in range(a,b):
+##      print(i,end=' ')
+      
+##a=int(input("Enter the number: "))
+##b=int(input("Enter the number: "))
+##for i in range(a,b+1):
+##      print(i,end=' ')
+
+##reversing the numbers from 1 to 10 to 10 to 1
+##
+##a,b=map(int,input().split())
+##for i in range(a,b):
+##     print(i,end=' ')
+
+##a=int(input("Enter the number: "))
+##b=int(input("Enter the number: "))
+##temp=a
+##a=b
+##b=temp
+##print("Before swapping the numbers are: ",b,a)
+##print("After swapping the numbers are: ",a,b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
