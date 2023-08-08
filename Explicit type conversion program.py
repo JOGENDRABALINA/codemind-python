4a99114f0d7633555d2974bdaaa99ffbf0c1be60
BALINA JOGENDRA VENKATA SIVA SUBRAHMANYAM <121618316+JOGENDRABALINA@users.noreply.github.com>
2023-08-08 21:30:30 +0530

    Add files via upload

---
+++ Explicit type conversion program.py
@@ -0,0 +1,33 @@
+##a = 5
+##b = "10"
+##c = 3.14
+##b_int = int(b)
+##c_int = int(c)
+##a_float = float(a)
+##c_str = str(c)
+##print("a =", a, "and has type", type(a))
+##print("b =", b, "and has type", type(b))
+##print("c =", c, "and has type", type(c))
+##print("b_int =", b_int, "and has type", type(b_int))
+##print("c_int =", c_int, "and has type", type(c_int))
+##print("a_float =", a_float, "and has type", type(a_float))
+##print("c_str =", c_str, "and has type", type(c_str))
+
+
+##name = "Jogendra"
+##age = 19
+##height = 5.11
+### Demonstrate various format specifiers
+##print(f"My name is {name}")
+##print(f"I am {age:d} years old")
+##print(f"I am {height:.2f} meters tall")
+##print(f"My name is {name:>10}")
+##print(f"My name is {name:<10}")
+##print(f"My name is {name:^10}")
+##print(f"My age is {age:+}")
+##print(f"My age is {age:-}")
+##print(f"My age is {age: }")
+##print(f"My age is {age:03}")
+
+
+
